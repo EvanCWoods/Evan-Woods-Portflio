f696b0212588fa2500479bb25d4f7c3ebd4abbae
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-06-09 21:00:53 +1000

    updated portfolio, updated projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,6 +8,7 @@ import mushroom from "../Assets/project-images/mushroom.jpeg";
 import cocktails from "../Assets/project-images/cocktails.png";
 import minesweeper from "../Assets/project-images/minesweeper.png";
 import genetic from "../Assets/project-images/genetic-algo.png";
+import cognizance from "../Assets/project-images/cognizance.png";
 
 function Projects() {
     return (
@@ -15,7 +16,7 @@ function Projects() {
             <Project image={artJacent} projectTitle="ArtJacent" projectDescription="This is a social media app that is directed at local artists and people who are interested in their work. Artists post their work, and the viewer sees them in their feed, sorted by distance to the creator." link="https://github.com/EvanCWoods/artJacent"/>
             <Project image={noteTaker} projectTitle="Note Taker" projectDescription="This is a note taker app that runs in the browser. It is designed to allow a user to create notes throughout the work day, change them as reuired and delete them upon completion." link="https://github.com/EvanCWoods/Note-Taker"/>
             <Project image={mushroom} projectTitle="Mushroom Classifier" projectDescription="This is a machine learning project which uses TensoFlow to detirmine whether a mushroom is poisonous or eddible based on a set of features such as color, size, stock length etc." link="https://github.com/EvanCWoods/Mushroom-Classifier"/>
-            <Project image={cocktails} projectTitle="Cocktail Translator" projectDescription="This app takes user input of a cocktail name, allows them to select the language to translate to, and provides information in both english and the selected language such as a description, ingredients and measurements." link="https://github.com/EvanCWoods/group-3-cocktails-in-any-language"/>
+            <Project image={cognizance} projectTitle="Cognizance" projectDescription="This app is a financial SaaS that allows subscribers to access comprehensive market data, that includes price, along with statistical and technical indicators. Subscribers can access this data either through the API, or the user interface." link="https://evan-woods-final-project.herokuapp.com"/>
             <Project image={minesweeper} projectTitle="Mine Sweeper game" projectDescription="This is a recreation of the once popular game 'Mine Sweeper'. It is built using JavaScript and emulates the origin game mechanics." link="https://github.com/EvanCWoods/mineSweeper"/>
             <Project image={genetic} projectTitle="Genetic Algorithm" projectDescription="This project uses raw python to simulate natural selection at its basic level. Essentially using numbers as genetic code to be passed on from generation to generation until the 'creatures' become very good at guessing a number." link="https://github.com/EvanCWoods/genetic-algorithm"/>
         </div>
